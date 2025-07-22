2b05c3024fdbfe3ebac4853cacad790e51e85887
Simas-pedro <pedrosimao171@gmail.com>
2025-07-22 12:06:40 -0400

    ajuste check icon

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,16 +1,16 @@
-import { ArrowRight, Trash2 } from "lucide-react";
+import { ArrowRight, CheckIcon, Trash2 } from "lucide-react";
 import { useNavigate } from "react-router";
 import Button from "./Button";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function onSeeDetailsClick(task) {
-    const query = new URLSearchParams()
+    const query = new URLSearchParams();
     // substitui o: --- E garante que não vá vir nada na string que não possa estar na url
     // navigate(`/task?title=${task.title}&description=${task.description}`);
-    query.set("title", task.title)
-    query.set("description", task.description)
+    query.set("title", task.title);
+    query.set("description", task.description);
     navigate(`/task?${query.toString()}`);
   }
   return (
@@ -20,15 +20,18 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
           {/*BOTÃO TAREFA*/}
           <Button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-gray-500/15 text-white w-full p-2 rounded-md shadow-lg hover:bg-gray-500/30 transition cursor-pointer active:bg-gray-500/0 active:transition-none ${
+            className={`bg-gray-500/15 text-white w-full p-2 rounded-md shadow-lg hover:bg-gray-500/30 transition cursor-pointer active:bg-gray-500/0 active:transition-none flex justify-center ${
               task.isCompleted ? "line-through opacity-40" : ""
             }`} // tem como colocar js dentro do tailwindcss, tipo aqui que se task tem isCompleted == true ele fica riscado e com baixa opacidade
           >
-            {task.title}
+            {task.isCompleted ? <CheckIcon /> : null} {/*Renderiza o Icone quando completo*/}
+            {task.title} {/*Renderiza o titulo da task*/}
           </Button>
+          
 
           {/*BOTÃO VER DETALHES*/}
-          <Button onClick={() => onSeeDetailsClick(task)}
+          <Button
+            onClick={() => onSeeDetailsClick(task)}
             className={`bg-gray-500/15 text-white p-2 rounded-md shadow-lg hover:bg-gray-500/40 transition cursor-pointer active:bg-gray-500/80 active:transition-none ${
               task.isCompleted ? "opacity-40" : ""
             }`}
